6bf09b7265d9cc37e59e1f496e378beeb5fc3494
sajit <sajit.kunnumkal@gmail.com>
2011-08-08 11:09:44 -0400

    Setting up project

--- src/main/java/org/ooloo/twitter/client/TwitterClient.java
+++ src/main/java/org/ooloo/twitter/client/TwitterClient.java
@@ -1,5 +1,16 @@
 package org.ooloo.twitter.client;
 
+
+import twitter4j.ResponseList;
+import twitter4j.Status;
+import twitter4j.TwitterException;
+
+
 public interface TwitterClient {
+	
+	/**
+	 * Get user's timeline
+	 */
+	public ResponseList<Status> getHomeTimeline() throws TwitterException;
 
 }
