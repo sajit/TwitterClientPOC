8af2a78829a79cec5485b5be32c73b60e8b92668
Sajit Mathew Kunnumkal <sajit.kunnumkal@gmail.com>
2011-08-09 08:20:33 -0400

    Draft3

---
+++ src/main/java/org/ooloo/twitter/client/config/DefaultTwitterConfig.java
@@ -0,0 +1,32 @@
+package org.ooloo.twitter.client.config;
+
+import java.util.Map;
+
+public class DefaultTwitterConfig implements TwitterClientConfig {
+
+	public boolean getFeature(String arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Map<String, Boolean> getFeatures() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Map<String, Object> getProperties() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getProperty(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean getPropertyAsFeature(String featureName) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
--- src/main/java/org/ooloo/twitter/client/config/TwitterClientConfig.java
+++ src/main/java/org/ooloo/twitter/client/config/TwitterClientConfig.java
@@ -3,5 +3,9 @@ package org.ooloo.twitter.client.config;
 import com.sun.jersey.core.util.FeaturesAndProperties;
 
 public interface TwitterClientConfig extends FeaturesAndProperties{
+	
+	public boolean getPropertyAsFeature(String featureName);
+	public static final String CONSUMER_KEY = "consumer.key";
+	public static final String CONSUMER_SECRET = "consumer.secret";
 
 }
